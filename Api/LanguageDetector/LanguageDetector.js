d6dc291c86fb046c3a8fb06c217aa67588022d71
Ranranruo <tlsalstjr58@gmail.com>
2025-08-05 14:22:06 +0900

    add: LanguageDetector
    LanguageDetector API 정리
    
    Resolve

---
+++ Api/LanguageDetector/LanguageDetector.js
@@ -0,0 +1,25 @@
+/**
+ * LanguageDetector API Chrome 에서 실험적 기능으로 지원하는 API이다
+ * 텍스트가 어느 언어인지 감지 할 수 있다.
+ * 
+ * 1. LanguageDetector.create() 메서드로 인스턴스를 할당한다. (Promise)
+ * 2. 인스턴스의 detect메서드를 사용하여 언어를 감지한다. (Promise)
+ * 
+ * detect 결과값은 [...{confidence, detectedLanguage}] 형태의 객체 배열이며 배열 요소는 감지한 언어 확률대로 정렬되어 있다.
+ * confidence: AI모델의 예측결과에 대한 확신도로 0~1값을 가지며 1에 가까울소록 모델의 예측이 정확할 것이라고 판단할 수 있다.
+ * detectedLanguage: 어떤 언인지 감지 결과 (ko, en, hi...)
+ */
+
+const detector = await LanguageDetector.create(); // 인스턴스 할당
+
+const korean = "한국어입니다."; // 감지할 텍스트
+const english = "It's english"; // 감지할 텍스트
+
+const koreanResult = await detector.detect(korean); // 감지
+const englishResult = await detector.detect(english); // 감지
+
+console.log(koreanResult[0].confidence); // 0.996...
+console.log(englishResult[0].confidence); // 0.996...
+
+console.log(koreanResult[0].detectedLanguage); // ko
+console.log(englishResult[0].detectedLanguage); // en
